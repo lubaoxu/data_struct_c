3ad7e4746ceb8717175872b2db13ca25c5611cff
lbx_super_man <1461608998@qq.com>
2022-08-14 19:04:12 +0800

    fsda

---
+++ summer-holidy-2022/8.14/file/file/file.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE* pFileToWrite = fopen("lbx.txt", "w");
+	char szData[] = "lbx \nHello world";
+	fwrite(szData, sizeof(szData), 1, pFileToWrite);
+	fclose(pFileToWrite);
+
+
+	FILE* pFileToRead = fopen("lbx.txt", "r");
+	char szDataRead[100] = {0};
+	fread(szDataRead, sizeof(szDataRead), 1, pFileToWrite);
+	printf("result\n%s\n", szData);
+	fclose(pFileToWrite);
+
+
+	return 0;
+}
